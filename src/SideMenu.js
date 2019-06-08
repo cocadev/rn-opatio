2e7c0093493612d4a141f5c149f7c65497949615
Jerry <40970351+funnyjerry@users.noreply.github.com>
2019-06-09 02:41:50 +0800

    add sideMenu

--- src/SideMenu.js
+++ src/SideMenu.js
@@ -4,6 +4,7 @@ import { StyleSheet, Platform, View, Text, Image, TouchableOpacity, Dimensions }
 import { Actions } from 'react-native-router-flux';
 import { colors } from './common/colors';
 import { images } from './common/images';
+import { p } from './common/normalize';
 
 const width = Dimensions.get('window').width
 
@@ -19,50 +20,37 @@ export default class SideMenu extends Component {
               
               <View style={{flex:3, justifyContent:'center', backgroundColor:'#eaf7f8'}}>
                  <TouchableOpacity onPress={()=>Actions.profile()} style={{flexDirection:'row', alignItems:'center', marginLeft: 25,marginTop: 15,}}>
-                     <Image source={images.img_user2} style={styles.avatar}/>
-                     <Text style={[{ marginLeft:16}]}>JANE DOE</Text>
+                     <Image source={images.user} style={styles.avatar}/>
+                     <Text style={[{ marginLeft:16, fontSize: p(16)}]}>Martin Garcia</Text>
                  </TouchableOpacity>
-                 <View style={{flexDirection:'row', alignItems:'flex-end', justifyContent:'flex-end', marginTop: 5, right:10}}>
-                     <Image source={images.icon_edit} style={{width:12, height:12, marginHorizontal:6}}/>
-                     <Text>EDIT</Text>
-                 </View>
+       
               </View>
 
-              <View style={{flex:12, backgroundColor:'#fff', paddingLeft:width/12}}>
-                <TouchableOpacity style={styles.menu} onPress={()=>Actions.wall()} >
-                    <Image source={images.icon_home} style={{width:15, height:15}}/>
-                    <Text style={[{marginLeft:22}]}>HOME</Text>
+              <View style={{flex:12, backgroundColor:'#fff', paddingLeft: p(20)}}>
+                <TouchableOpacity style={styles.menu}>
+                    <Text style={styles.text}>Configuracion de cuenta</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.menu} onPress={()=>Actions.orderlist()}>
-                    <Image source={images.icon_sheet} style={{width:15, height:15}}/>
-                    <Text style={[{marginLeft:22}]}>ORDERS</Text>
+                <TouchableOpacity style={styles.menu}>
+                    <Text style={styles.text}>Informacion de cuenta</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={()=>Actions.promotion()} style={styles.menu}>
-                    <Image source={images.icon_gift} style={{width:15, height:15}}/>
-                    <Text style={[{marginLeft:22}]}>PROMOTIONS</Text>
+                <TouchableOpacity style={styles.menu}>
+                    <Text style={styles.text}>Cambiar direction de Email</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={()=>Actions.serviceprovider()} style={styles.menu}>
-                    <Image source={images.icon_setting} style={{width:15, height:15}}/>
-                    <Text style={[{marginLeft:22}]}></Text>
+                <TouchableOpacity style={styles.menu}>
+                    <Text style={styles.text}>Cambiar contrasena</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.menu} onPress={()=>Actions.legal()}>
-                    <Image source={images.icon_doc} style={{width:14, height:16}}/>
-                    <Text style={[{marginLeft:22}]}>LEGAL</Text>
+                <TouchableOpacity style={styles.menu}>
+                    <Text style={styles.text}>Information de pagos</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={()=>Actions.invite()} style={styles.menu}>
-                    <Image source={images.icon_users} style={{width:17, height:15}}/>
-                    <Text style={[{marginLeft:22}]}>INVITE YOUR FRIEND</Text>
+                <TouchableOpacity style={styles.menu}>
+                    <Text style={styles.text}>Invitar usuarios</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={()=>Actions.signin()} style={styles.menu}>
-                    <Image source={images.icon_roundback} style={{width:20, height:15}}/>
-                    <Text style={[{marginLeft:20}]}>LOGOUT</Text>
+                <TouchableOpacity style={styles.menu}>
+                    <Text style={styles.text}>Configuracion de reportes</Text>
                 </TouchableOpacity>
-              </View>
-
-              <View style={{flex:2, justifyContent:'center', alignItems:'center', backgroundColor:'#eaf7f8'}}>
-                 <View style={styles.button}>
-                     <Text>BECOME A SERVICE PROVIDER</Text>
-                 </View>
+                <TouchableOpacity style={styles.menu}>
+                    <Text style={[styles.text, { fontWeight: '600'}]}>Log out</Text>
+                </TouchableOpacity>
               </View>
             
             </View>
@@ -71,20 +59,11 @@ export default class SideMenu extends Component {
 }
 
 const styles = StyleSheet.create({
-
     container: {
         flex: 1,
         flexDirection: 'column',
         backgroundColor: '#111',
         paddingTop: 24,
-    },
-    button:{
-        backgroundColor:colors.RED, 
-        justifyContent:'center', 
-        alignItems: 'center', 
-        borderRadius:20,
-        paddingVertical: 12,
-        paddingHorizontal: width/10,
     },
     avatar:{
         width:width/6,
@@ -94,9 +73,13 @@ const styles = StyleSheet.create({
     menu:{
         flexDirection:'row', 
         alignItems:'center',
-        paddingVertical:width/22, 
+        paddingVertical: p(14), 
         borderBottomColor: colors.GREY4, 
         borderBottomWidth: 1,
+    },
+    text: {
+        fontSize: p(14),
+        marginLeft: p(22)
     }
 
 });
--- src/common/images.js
+++ src/common/images.js
@@ -2,5 +2,6 @@ export const images = {
 
     logo: require('../../assets/logo.png'),
     intro: require('../../assets/images/intro.png'),
+    user: require('../../assets/images/user.jpg'),
 
 };
