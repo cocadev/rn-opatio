1fbb558eb537026f0e01b484924a772d1ab67741
Wang Harry <eugene19950901@outlook.com>
2019-06-11 16:31:19 +0800

    ZIndexMarkers with tons of JSON

--- src/router.js
+++ src/router.js
@@ -16,7 +16,7 @@ import Lotes from './screens/Lotes/Lotes';
 
 import { images } from './common/images';
 import Test from './screens/Map/test';
-import Overlays from './screens/Map/ViewsAsMarkers';
+import Overlays from './screens/Map/ZIndexMarkers';
 
 const width = Dimensions.get('window').width
 const height = Dimensions.get('window').height
