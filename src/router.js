ba0360d81cb771fdee2692acdd88806ea3a4c93e
Wang Harry <eugene19950901@outlook.com>
2019-06-11 16:27:07 +0800

    PolylineCreator

--- src/router.js
+++ src/router.js
@@ -16,7 +16,7 @@ import Lotes from './screens/Lotes/Lotes';
 
 import { images } from './common/images';
 import Test from './screens/Map/test';
-import Overlays from './screens/Map/PolygonCreator';
+import Overlays from './screens/Map/PolylineCreator';
 
 const width = Dimensions.get('window').width
 const height = Dimensions.get('window').height
